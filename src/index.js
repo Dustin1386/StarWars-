f98c2fdffd2e8616017d1f46d22263357c420543
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-26 12:38:19 -0400

    added deploy scripts

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Config from './config.js';
+
+console.log(Config.API_KEY);
 
 ReactDOM.render(<App />, document.getElementById('root'));
